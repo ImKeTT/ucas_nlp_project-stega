58f894852e30ec6c8890c2a574868c0f2ca28590
ImKeTT <thq415_ic@yeah.net>
2021-12-03 16:13:19 +0800

    update steamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -211,20 +211,20 @@ def extract(stego, model_name="VAE", mode="arithmetic"):
     return reconst
 
 if __name__=="__main__":
-    # desc = "Uses RNN/VAE/GPT-2 with Huffman and Arithmetic Coding to hide or extract secret messages. Check out the code and corresponding papers [here](https://github.com/ImKeTT/ucas_nlp_project_stega)!"
-    # st.title('Linguistic Generative Steganography')
-    # st.write(desc)
-    # model_name = st.selectbox('Select a Language Model for Hiding', ('GPT_2', 'RNN', 'VAE'))
-    # method = st.selectbox('Select a Coding Method for Hiding', ('huffman', 'arithmetic'))
-    # secret_message = st.text_input('Seceret Message (cannot leave blank)')
-    # if st.button('Hide Message'):
-    #     stego = main(secret_message, model_name, method)
-    #     st.write(stego)
-    # model_name = st.selectbox('Select a Language Model for Extracting', ('GPT_2', 'RNN', 'VAE'))
-    # method = st.selectbox('Select a Coding Method for Extracting', ('huffman', 'arithmetic'))
-    # stego = st.text_input('Message to be Extracted (cannot leave blank)')
-    # if st.button('Extract Message'):
-    #     stego = main(secret_message, model_name, method)
-    #     st.write(stego)
-    stego = main("this is a secret", "RNN", "huffman")
-    print(stego)
+    desc = "Uses RNN/VAE/GPT-2 with Huffman and Arithmetic Coding to hide or extract secret messages. Check out the code and corresponding papers [here](https://github.com/ImKeTT/ucas_nlp_project_stega)!"
+    st.title('Linguistic Generative Steganography')
+    st.write(desc)
+    model_name = st.selectbox('Select a Language Model for Hiding', ('GPT_2', 'RNN', 'VAE'))
+    method = st.selectbox('Select a Coding Method for Hiding', ('huffman', 'arithmetic'))
+    secret_message = st.text_input('Seceret Message (cannot leave blank)')
+    if st.button('Hide Message'):
+        stego = main(secret_message, model_name, method)
+        st.write(stego)
+    model_name = st.selectbox('Select a Language Model for Extracting', ('GPT_2', 'RNN', 'VAE'))
+    method = st.selectbox('Select a Coding Method for Extracting', ('huffman', 'arithmetic'))
+    stego = st.text_input('Message to be Extracted (cannot leave blank)')
+    if st.button('Extract Message'):
+        stego = main(secret_message, model_name, method)
+        st.write(stego)
+    # stego = main("this is a secret", "RNN", "huffman")
+    # print(stego)
